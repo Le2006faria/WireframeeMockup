6406f22b9d11fe1cb4ce8998578c8392db70abfe
Le2006faria <le2006faria@gmail.com>
2023-10-25 14:01:15 -0300

    html

---
+++ js/index.js
@@ -0,0 +1,41 @@
+function cor1(){
+    let cor = document.getElementById(`cor1`);
+    cor.style.color = `#eff3cd`;
+    cor.style.backgroundColor = `#605063`;
+}
+
+function cor2(){
+    let cor = document.getElementById(`cor2`);
+    cor.style.color = `#eff3cd`;
+    cor.style.backgroundColor = `#605063`;
+}
+
+function cor3(){
+    let cor = document.getElementById(`cor3`);
+    cor.style.color = `#eff3cd`;
+    cor.style.backgroundColor = `#605063`;
+}
+
+function cor4(){
+    let cor = document.getElementById(`cor4`);
+    cor.style.color = `#eff3cd`;
+    cor.style.backgroundColor = `#605063`;
+}
+
+function cor5(){
+    let cor = document.getElementById(`cor5`);
+    cor.style.color = `#eff3cd`;
+    cor.style.backgroundColor = `#605063`;
+}
+
+function cor6(){
+    let cor = document.getElementById(`cor6`);
+    cor.style.color = `#eff3cd`;
+    cor.style.backgroundColor = `#605063`;
+}
+
+function cor7(){
+    let cor = document.getElementById(`cor7`);
+    cor.style.color = `#eff3cd`;
+    cor.style.backgroundColor = `#605063`;
+}
