5ebc295557ac9c8da4b00870ff5aa5daf6f7bbf3
Le2006faria <le2006faria@gmail.com>
2023-11-10 13:44:27 -0300

    .

--- js/index.js
+++ js/index.js
@@ -17,5 +17,29 @@ function cadastrar() {
     console.log(dados);
   
       // Requisição para back-end
-  
+
+      // Faz requisição ao servidor usando o verbo POST, enviando os dados para o servidor
+fetch(`${url_server}/cadastro`, {
+  // define o uso do método POST
+  method: 'POST',
+  // informações sobre o cabeçalho da requisição
+  headers: {
+      'Content-Type': 'application/json'
+  },
+  // transforma os dados da requisição em um JSON chamado body
+  body: JSON.stringify(dados)
+})
+  // Depois de feita a requisição, o front-end irá receber um retorno do servidor
+  .then(response => response.json())
+   // Se toda a requisição deu certo, será informado no log
+   .then(dados => {
+      console.log('Resposta do servidor:', dados);
+      // Faça algo com a resposta do servidor, se necessário
+   })
+   // Caso tenha algum tipo de erro na requisição, é lançada a excessão
+   .catch(error => {
+       console.error('Erro ao enviar dados para o servidor:', error);
+       // Trate os erros, se necessário
+   });
+   
   }
